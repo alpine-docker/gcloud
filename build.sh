fe12863bbd509c50cac3563502c5cc60648a0b15
Bill Wang <bill.wang>
2020-04-29 13:45:21 +1000

    set automation build

--- build.sh
+++ build.sh
@@ -10,15 +10,27 @@
 
 image="alpine/gcloud"
 
+sum=0
+
 latest=$(curl -sL curl https://cloud.google.com/sdk/docs/release-notes |awk -F ">" '/tabindex/{print $2}' |awk '{print $1}' |head -1)
-echo $latest
+echo "Lastest release is: ${latest}"
 
-docker build -t ${image}  --build-arg VERSION=${latest} .
-docker tag ${image} ${image}:${latest}
+tags=`curl -s https://hub.docker.com/v2/repositories/${image}/tags/ |jq -r .results[].name`
 
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
- docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
- docker push ${image}
- docker push ${image}:${latest}
+for tag in ${tags}
+do
+  if [ ${tag} == ${latest} ];then
+    sum=$((sum+1))
+  fi
+done
+
+if [[ ( $sum -ne 1 ) || ( ${REBUILD} == "true" ) ]];then
+  docker build -t ${image}  --build-arg VERSION=${latest} .
+  docker tag ${image} ${image}:${latest}
+
+  if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+   docker push ${image}
+   docker push ${image}:${latest}
+  fi
 fi
-
